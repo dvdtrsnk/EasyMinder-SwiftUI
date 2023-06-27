1a77a3cbb05eecbcada013e8c725d384b0a3ee4b
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-06-27 10:15:35 +0200

    Initial Commit

---
+++ EasyMinder-SwiftUI/EasyMinder_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMinder_SwiftUIApp.swift
+//  EasyMinder-SwiftUI
+//
+//  Created by David Třešňák on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyMinder_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
